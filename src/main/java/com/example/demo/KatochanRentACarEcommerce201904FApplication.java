1594b880592a3e8fe2b5eaaf99ed8c043ecc91ce
jun.makino <jun.makino@rakus.co.jp>
2019-05-31 18:27:58 +0900

    first commit

---
+++ src/main/java/com/example/demo/KatochanRentACarEcommerce201904FApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatochanRentACarEcommerce201904FApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatochanRentACarEcommerce201904FApplication.class, args);
+	}
+
+}
