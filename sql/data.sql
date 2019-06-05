d21db43b579a8541ac0ad3442b7bed92b4998baa
ryokato <ryo.kato@rakus.co.jp>
2019-06-05 14:45:51 +0900

    車種の画像を追加

--- sql/data.sql
+++ sql/data.sql
@@ -1,15 +1,15 @@
 insert into users (name, email, password, address, telephone)
 values ('山田太郎', 'test@test.co.jp', 'password', '東京都新宿区新宿4町目', '0120-123-123');
 
-insert into cars values(1, 'デミオ', '1', '1', '1.jpg');
-insert into cars values(2, 'ヴィッツ', '1', '2', '2.jpg');
-insert into cars values(3, 'マーチ', '1', '3', '3.jpg');
-insert into cars values(4, 'レガシィ', '2', '1', '4.jpg');
-insert into cars values(5, 'アテンザ', '2', '2', '5.jpg');
-insert into cars values(6, 'フーガ', '2', '3', '6.jpg');
-insert into cars values(7, 'ロードスター', '3', '1', '7.jpg');
-insert into cars values(8, 'BRZ', '3', '2', '8.jpg');
-insert into cars values(9, '86', '3', '3', '9.jpg');
+insert into cars values(1, 'デミオ', '1', '1', 'demio.jpg');
+insert into cars values(2, 'ヴィッツ', '1', '2', 'vitz.jpg');
+insert into cars values(3, 'マーチ', '1', '3', 'march.jpg');
+insert into cars values(4, 'レガシィ', '2', '1', 'legacy.jpg');
+insert into cars values(5, 'アテンザ', '2', '2', 'atenza.jpg');
+insert into cars values(6, 'フーガ', '2', '3', 'fuga.jpg');
+insert into cars values(7, 'ロードスター', '3', '1', 'roadster.jpg');
+insert into cars values(8, 'BRZ', '3', '2', 'brz.jpg');
+insert into cars values(9, '86', '3', '3', '86.jpg');
 
 insert into grades values(1, 'コンパクトカー', '2000');
 insert into grades values(2, 'セダン', '3000');
