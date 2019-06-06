3e7761c764deb4e801d0681f1f836fd298c50433
jun.makino <jun.makino@rakus.co.jp>
2019-06-06 22:12:32 +0900

    パスワード暗号化実装

--- src/main/java/jp/co/rakus/ecommerce/repository/AccountRepository.java
+++ src/main/java/jp/co/rakus/ecommerce/repository/AccountRepository.java
@@ -5,34 +5,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import jp.co.rakus.ecommerce.domain.Account;
 
 @Repository
 public class AccountRepository {
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
-	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 //	ユーザ情報を保存
 	public Account save(Account account) {
+		String password = passwordEncoder.encode(account.getPassword());
+		account.setPassword(password);
 		SqlParameterSource param=new BeanPropertySqlParameterSource(account);
-		
+
 		if (account.getId() == null) {
-			jdbcTemplate.update(		
-					"INSERT INTO users(name,address,telephone,email,password) values(:name,:address,:telephone,:email,:password)", 
+			jdbcTemplate.update(
+					"INSERT INTO users(name,address,telephone,email,password) values(:name,:address,:telephone,:email,:password)",
 					param);
-			
-				
+
+
 		} else {
 			jdbcTemplate.update(
-					"UPDATE users SET name=:name,address=:address,telephone=:telephone,email=:email,password=:password, WHERE id=:id", 
+					"UPDATE users SET name=:name,address=:address,telephone=:telephone,email=:email,password=:password, WHERE id=:id",
 					param);
 
 		}
 			return account;
 	}
-	
-	
+
+
 }
--- src/main/java/jp/co/rakus/ecommerce/repository/UserRepository.java
+++ src/main/java/jp/co/rakus/ecommerce/repository/UserRepository.java
@@ -10,75 +10,81 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import jp.co.rakus.ecommerce.domain.User;
 
 @Repository
 @Transactional
 public class UserRepository {
-	
+
 	private static final RowMapper<User> userRowMapper=(rs,i) ->{
 		Integer id=rs.getInt("id");
 		String name=rs.getString("name");
 		String email=rs.getString("email");
-		String password=rs.getString("password");
 		String address=rs.getString("address");
+		String password=rs.getString("password");
 		String telephone=rs.getString("telephone");
-		return new User(id,name,email,password,address,telephone);
+		return new User(id,name,email,address,password,telephone);
+		// Integer id, String name, String email, String address, String password, String telephone
 	};
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate jdbctemplate;
-	
-	
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 //	メールアドレス、パスワードからメンバー取得
 	public User findByEmailAndPassword(String email,String password) {
 		SqlParameterSource param=new MapSqlParameterSource()
 				.addValue("email",email)
 				.addValue("password",password);
 		User user=null;
-		
+
 		try {
 			user=jdbctemplate.queryForObject(
-					"SELECT id,name,email,address,password,telephone FROM users WHERE email=:email AND password=:password",
+					"SELECT id,name,email,address,password,telephone FROM users WHERE email=:email",
 					param,userRowMapper);
-			
-			return user;
+			if (passwordEncoder.matches(password, user.getPassword())) {
+				return user;
+			} else {
+				return null;
+			}
 		} catch(DataAccessException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	
 
-	
+
+
+
 //	ユーザの新規登録
 	public User save(User user) {
 		SqlParameterSource param=new BeanPropertySqlParameterSource(user);
-		
+
 		if(user.getId()==null) {
 			String insertSql="INSERT INTO user(name,email,password,address,telephone)"
 								+"VALUES(:name,:email,:password,:address,:telephone)";
-			
+
 			jdbctemplate.update(insertSql,param);
 		}else{
 			String updateSql="UPDATE user SET name=:name,email=:email,password=:password,"
 								+ "address=:address,:telephone=:telephone";
 		jdbctemplate.update(updateSql, param);
 		}
-		
+
 		return user;
 	}
-	
+
 	public void deleteById(Integer id) {
 		String deleteSql="DELETE FROM users WHERE id=:id";
-		
+
 		SqlParameterSource param=new MapSqlParameterSource().addValue("id",id);
-		
+
 		jdbctemplate.update(deleteSql,param);
 	}
 
